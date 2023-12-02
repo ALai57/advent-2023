c0b46387f7070e35cf832b9c9eab1e4496d2bddf
Andrew Lai <andrew.s.lai5@gmail.com>
2023-12-01 20:36:05 -0600

    Refactor

--- clojure/src/day1.clj
+++ clojure/src/day1.clj
@@ -26,7 +26,13 @@
    "eight" "8"
    "nine"  "9"})
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; String matching
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defn all-matches
+  "Finds all matches for a regular expression, allowing for overlap.
+  i.e. (all-matches #\"zero|one\" \"zerone\") => [\"zero\" \"one\"]
+  See https://www.regular-expressions.info/lookaround.html for more details"
   [re-string s]
   (map second
        (-> (format "(?=(%s))"
@@ -34,52 +40,30 @@
            re-pattern
            (re-seq s))))
 
-(defn all-numeric-string-matches
-  [s]
-  (let [dict NUMERIC-STRINGS]
-    (map (partial get dict) (all-matches (string/join "|" (keys dict)) s))))
-
-(defn all-numeric-matches
-  [s]
-  (let [dict (merge NUMERIC-STRINGS NUMERIC-WORDS)]
-    (map (partial get dict) (all-matches (string/join "|" (keys dict)) s))))
-
-
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; String matching
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (def scrambled-rows
   (-> "./resources/day1.txt"
       io/file
       slurp
       (string/split #"\n")))
 
-scrambled-rows
-;; => ["1abc2" "pqr3stu8vwx" "a1b2c3d4e5f" "treb7uchet"]
-
-
-
-(comment
-  (all-numeric-string-matches "12 33cc 2")
-  ;; => ("1" "2" "3" "3" "2")
-
-  (all-numeric-matches "zerone11twox32")
-  ;; => ("zero" "one" "1" "1" "two" "3" "2")
-  )
-
 (defn calibration-value
-  [match-fn s]
-  (let [ms (match-fn s)]
+  [dictionary s]
+  (let [ms (->> s
+                (all-matches (string/join "|" (keys dictionary)))
+                (map (partial get dictionary)))]
     (str (first ms) (last ms))))
 
 (def part1-answer
   (->> scrambled-rows
-       (map (comp parse-long (partial calibration-value all-numeric-string-matches)))
-       (reduce + )))
+       (map (partial calibration-value NUMERIC-STRINGS))
+       (map parse-long)
+       (reduce +)))
 
 (def part2-answer
   (->> scrambled-rows
-       (map (comp parse-long (partial calibration-value all-numeric-matches)))
+       (map (partial calibration-value (merge NUMERIC-STRINGS NUMERIC-WORDS)))
+       (map parse-long)
        (reduce +)))
-
-(comment
-  (calibration-value all-numeric-matches (first scrambled-rows))
-  )
